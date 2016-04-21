ba0953a372cf7031b0209665a8757618d8f40556
TheGreatGooo <thegreatgooo@gmail.com>
2016-04-21 10:29:22 -0400

    Update gpg-mailgate.py
    decode payload before encryption

--- gpg-mailgate.py
+++ gpg-mailgate.py
@@ -437,7 +437,7 @@ def encrypt_all_payloads_mime( message, gpg_to_cmdline ):
 		# it's skipping an imaginary blank line someplace. (ie skipping a header)
 		# Workaround it here by prepending a blank line.
 		# This happens only on text only messages.
-		submsg2.set_payload("\n" + message.get_payload())
+		submsg2.set_payload("\n" + message.get_payload(decode=True))
 		check_nested = True
 	else:
 		processed_payloads = generate_message_from_payloads(message)
